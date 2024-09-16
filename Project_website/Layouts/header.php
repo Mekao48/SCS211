bf363d49b264e9740667adefbe7d5efbb740e1f5
Mekao48 <84495852+Mekao48@users.noreply.github.com>
2024-09-17 04:03:37 +0700

    nav update

--- Project_website/Layouts/header.php
+++ Project_website/Layouts/header.php
@@ -1,100 +1,172 @@
-
-<nav class="navbar navbar-expand-lg " style="background-color: #164d64; padding: 10px;">
-    <div class="container-fluid">
-    <a class="navbar-brand" 
-        href="index.php">
-        <img src="asset/Logo.png" 
-        class="rounded-circle border border-white border-5" style="width: 75%; padding: 10px;">
+<nav class="navbar navbar-expand-lg sticky-top" style="background-color: #164d64; padding: 8px; opacity: 99%;">
+  <div class="container-fluid">
+    <a class="navbar-brand"
+      href="index.php">
+      <img src="asset/Logo.png"
+        class="rounded-circle border border-white border-5 d-xl-block d-lg-none d-md-none d-sm-none d-none" style="width: 70%; padding: 5px; margin-left: 45px;">
     </a>
+    <img src="asset/Logo.png"
+        class="rounded-circle border border-white border-5 d-xl-none d-lg-block d-md-none d-sm-none d-none" style="width: 120px; padding: 5px; margin-left: -20px; margin-right: 10px;">
+    </a>
+    <img src="asset/Logo.png"
+        class="rounded-circle border border-white border-5 d-lg-none d-md-block" style="width: 120px; padding: 5px; margin-left: -15px;">
+    </a>
+    <a href="index.php" class="d-lg-none d-md-block link-offset-3 link-underline-opacity-50" style="margin-right: 20px; margin-left: 10px; font-weight: bold; color: azure; font-size: xx-large;">อบต.บ้านควน</a>
     <button
-          class="navbar-toggler"
-          type="button"
-          data-bs-toggle="collapse"
-          data-bs-target="#navbarSupportedContent"
-          aria-controls="navbarSupportedContent"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
-        >
-          <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarSupportedContent">
-          <ul class="navbar-nav mx-auto mb-2 mb-lg-0">
+      class="navbar-toggler"
+      type="button"
+      data-bs-toggle="collapse"
+      data-bs-target="#navbarSupportedContent"
+      aria-controls="navbarSupportedContent"
+      aria-expanded="false"
+      aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav mx-auto mb-3 mb-lg-0 ms-1">
+      <div class="col">
+        <div class="row" style="margin-bottom: -13px; margin-top: 10px;">
+          <div class="col-auto">
             <li class="nav-item">
-              <a class="nav-link active" aria-current="page" href="index.php" style="color: white; font-size: 150%;">หน้าแรก</a>
+              <a class="nav-link active" aria-current="page" href="index.php" style="color: white; font-size: 175%;"><i class="bi bi-house" style="margin-right: 8px;"></i>หน้าแรก</a>
             </li>
-            <li class="nav-item dropdown">
-              <a
-                class="nav-link dropdown-toggle"
-                href="#"
-                role="button"
-                data-bs-toggle="dropdown"
-                aria-expanded="false"
-                style="color: white; font-size: 150%;"
-              >
-                ข้อมูลพื้นฐาน
-              </a>
-              <ul class="dropdown-menu" style="background-color: white;">
-                <li><a class="dropdown-item" href="#">ประวัติความเป็นมา</a></li>
-                <li><a class="dropdown-item" href="#">วิสัยทัศน์/พันธกิจ</a></li>
-                <li><a class="dropdown-item" href="#">โครงสร้างองค์กร</a></li>
-                <li><a class="dropdown-item" href="#">อำนาจหน้าที่</a></li>
-                <li><a class="dropdown-item" href="#">นโยบายการบริหาร</a></li>
-                <li><a class="dropdown-item" href="#">นโยบายคุ้มครองข้อมูลส่วนบุคคล</a></li>
-              </ul>
+              </div>
+              <div class="col-auto">
+            <li class="nav-item">
+              <a class="nav-link" aria-current="page" href="#" style="color: white; font-size: 125%;">กิจกรรม</a>
             </li>
-            <li class="nav-item dropdown">
-              <a
-                class="nav-link dropdown-toggle"
-                href="#"
-                role="button"
-                data-bs-toggle="dropdown"
-                aria-expanded="false"
-                style="color: white; font-size: 150%;"
-              >
-                แผนพัฒนา
-              </a>
-              <ul class="dropdown-menu" style="background-color: white;">
-                <li><a class="dropdown-item" href="#">แผนพัฒนาท้องถิ่น</a></li>
-                <li><a class="dropdown-item" href="#">แผนดำเนินงานประจำปี</a></li>
-                <li><a class="dropdown-item" href="#">รายงานผลการติดตามและประเมินผล</a></li>
-                <li><a class="dropdown-item" href="#">แผนปฏิบัติการป้องกันการทุจริตสี่ปี</a></li>
-                <li><a class="dropdown-item" href="#">แผนอัตรากำลัง</a></li>
-                <li><a class="dropdown-item" href="#">แผนพัฒนาบุคลากร</a></li>
-                <li><a class="dropdown-item" href="#">แผนบริหารความต่อเนื่อง BCP</a></li>
-              </ul>
+              </div>
+              <div class="col-auto">
+            <li class="nav-item">
+              <a class="nav-link" aria-current="page" href="#" style="color: white; font-size: 125%;">ข่าวประชาสัมพันธ์</a>
             </li>
-            
-          </ul>
-          <form class="d-flex" role="form">
-            <div class="column" style="margin-bottom: 10px; ">
-                <div class="row mb-2">
-            <input
-              class="form-control me-2"
-              type="text"
-              placeholder="ชื่อผู้ใช้"
-              aria-label="ชื่อผู้ใช้"
-              style="height: 20%; width: 90%;"
-            />
-            
-            </div>
-            <div class="row mb-2">
-            <input
-             class="form-control me-2"
-             type="password"
-             placeholder="รหัสผ่าน"
-             aria-label="รหัสผ่าน"
-             style="height: 50%; width: 90%;"
-             />
-             </div>
-            </div>
-            <button class="btn btn-outline-success" type="submit" style="height: 40px;">
-              เข้าสู่ระบบ
-            </button>
-          </form>
+              </div>
+              <div class="col-auto">
+            <li class="nav-item">
+              <a class="nav-link" aria-current="page" href="#" style="color: white; font-size: 125%;">ประกาศการจัดซื้อฯ</a>
+            </li>
+              </div>
         </div>
+    <hr class="border border-white border-2 opacity-75">
+        <div class="row ms-1-lg" style="margin-top: -10px;">
+          <div class="col-auto">
+        <li class="nav-item dropdown">
+          <a
+            class="nav-link dropdown-toggle"
+            href="#"
+            role="button"
+            data-bs-toggle="dropdown"
+            aria-expanded="false"
+            style="color: white; font-size: 125%;">
+            ข้อมูลพื้นฐาน
+          </a>
+          <ul class="dropdown-menu dropdown-menu-md-end" style="background-color: white;">
+            <li><a class="dropdown-item" href="#">ประวัติความเป็นมา</a></li>
+            <li><a class="dropdown-item" href="#">วิสัยทัศน์/พันธกิจ</a></li>
+            <li><a class="dropdown-item" href="#">โครงสร้างองค์กร</a></li>
+            <li><a class="dropdown-item" href="#">อำนาจหน้าที่</a></li>
+            <li><a class="dropdown-item" href="#">นโยบายการบริหาร</a></li>
+            <li><a class="dropdown-item" href="#">นโยบายคุ้มครองข้อมูลส่วนบุคคล</a></li>
+          </ul>
+        </li>
+          </div>
+          <div class="col-auto">
+        <li class="nav-item dropdown">
+          <a
+            class="nav-link dropdown-toggle"
+            href="#"
+            role="button"
+            data-bs-toggle="dropdown"
+            aria-expanded="false"
+            style="color: white; font-size: 125%;">
+            แผนพัฒนา
+          </a>
+          <ul class="dropdown-menu dropdown-menu-md-end" style="background-color: white;">
+            <li><a class="dropdown-item" href="#">แผนพัฒนาท้องถิ่น</a></li>
+            <li><a class="dropdown-item" href="#">แผนดำเนินงานประจำปี</a></li>
+            <li><a class="dropdown-item" href="#">รายงานผลการติดตามและประเมินผล</a></li>
+            <li><a class="dropdown-item" href="#">แผนปฏิบัติการป้องกันการทุจริตสี่ปี</a></li>
+            <li><a class="dropdown-item" href="#">แผนอัตรากำลัง</a></li>
+            <li><a class="dropdown-item" href="#">แผนพัฒนาบุคลากร</a></li>
+            <li><a class="dropdown-item" href="#">แผนบริหารความต่อเนื่อง BCP</a></li>
+          </ul>
+        </li>
+          </div>
+          <div class="col-auto">
+        <li class="nav-item dropdown">
+          <a
+            class="nav-link dropdown-toggle"
+            href="#"
+            role="button"
+            data-bs-toggle="dropdown"
+            aria-expanded="false"
+            style="color: white; font-size: 125%;">
+            การร้องเรียนการทุจริตฯ
+          </a>
+          <ul class="dropdown-menu dropdown-menu-md-end" style="background-color: white;">
+            <li><a class="dropdown-item" href="#">แนวปฏิบัติการจัดการเรื่องร้องเรียนฯ</a></li>
+            <li><a class="dropdown-item" href="#">ช่องทางแจ้งเรื่องร้องเรียนการทุจริตฯ</a></li>
+            <li><a class="dropdown-item" href="#">ข้อมูลสถิติเรื่องร้องเรียนฯ</a></li>
+            <li><a class="dropdown-item" href="#">การเปิดโอกาสให้เกิดการมีส่วนร่วม</a></li>
+            <li><a class="dropdown-item" href="#">การประเมินจริยธรรมเจ้าหน้าที่ของรัฐ</a></li>
+            <li><a class="dropdown-item" href="#">มาตรการส่งเสริมความโปร่งใสภายในหน่วยงาน</a></li>           
+          </ul>
+        </li>
+          </div>
+          <div class="col-auto">
+        <li class="nav-item dropdown">
+          <a
+            class="nav-link dropdown-toggle"
+            href="#"
+            role="button"
+            data-bs-toggle="dropdown"
+            aria-expanded="false"
+            style="color: white; font-size: 125%;">
+            รายงาน
+          </a>
+          <ul class="dropdown-menu dropdown-menu-md-end" style="background-color: white;">
+            <li><a class="dropdown-item" href="#">รายงานการคลัง</a></li>
+            <li><a class="dropdown-item" href="#">รายงานผลการดำเนินงาน</a></li>
+            <li><a class="dropdown-item" href="#">รายงานการประชุม</a></li>
+            <li><a class="dropdown-item" href="#">รายงานการติดตาม และประเมินผล</a></li>
+            <li><a class="dropdown-item" href="#">รายงานผลการประเมิน ITA</a></li>
+            <li><a class="dropdown-item" href="#">รายงานผลการประเมิน LPA</a></li>
+            <li><a class="dropdown-item" href="#">รายงานผลตามนโยบาย No Gift Policy</a></li>
+            <li><a class="dropdown-item" href="#">รายงานข้อมูลเชิงสถิติการให้บริการ</a></li>
+            <li><a class="dropdown-item" href="#">รายงานผลสำรวจความพึงพอใจการให้บริการ</a></li>
+            <li><a class="dropdown-item" href="#">รายงานผลประเมินเวทีเสวนาฯ</a></li>
+            <li><a class="dropdown-item" href="#">รายงานของกิจการสภา</a></li>
+            <li><a class="dropdown-item" href="#">รายงานผลการดำเนินการเพื่อส่งเสริมคุณธรรมและความโปร่งใสภายในหน่วยงาน</a></li>
+            <li><a class="dropdown-item" href="#">รายงานการรับทรัพย์สินหรือประโยชน์อื่นใดโดยธรรมจรรยา</a></li>
+            <li><a class="dropdown-item" href="#">รายงานผลการดำเนินการป้องกันการทุจริตประจำปี</a></li>
+          </ul>
+        </li>
+          </div>
+        </div>
+        </div>
+      </ul>
+      <div class="dropdown ">
+        <button type="button" class="btn btn-outline-success" data-bs-toggle="dropdown" aria-expanded="true" data-bs-auto-close="outside" style="font-weight: bolder; margin-bottom: 70px;">
+          <i class="bi bi-person-circle" style="margin-right: 5px;"></i>
+          เข้าสู่ระบบ
+        </button>
+        <form class="dropdown-menu p-4 dropdown-menu-lg-end shadow-lg" style="width: max-content; position: absolute; margin-top: -60px;">
+          <div class="mb-3">
+            <label for="Username" class="form-label">ชื่อผู้ใช้</label>
+            <input type="text" class="form-control" id="Username" placeholder="Username">
+          </div>
+          <div class="mb-3">
+            <label for="password" class="form-label">รหัสผ่าน</label>
+            <input type="password" class="form-control" id="password" placeholder="Password">
+          </div>
+          
+          <button type="submit" class="btn btn-primary">Login</button>
+        </form>
+      </div>
     </div>
+  </div>
 </nav>
 <!-- ฺAnoucement -->
 <div style="background-color: white;">
-    <h2>(ประกาศ)</h2>
+  <h2>(ประกาศ)</h2>
 </div>
