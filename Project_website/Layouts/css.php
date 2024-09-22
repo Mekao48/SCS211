4df11b11a41b85afe0121a6d6d16a5726f09d199
Mekao48 <84495852+Mekao48@users.noreply.github.com>
2024-09-22 14:50:31 +0700

    history update

--- Project_website/Layouts/css.php
+++ Project_website/Layouts/css.php
@@ -1,5 +1,5 @@
 <title>Bankuan</title>
-<link
+  <link
       rel="stylesheet"
       href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css"
     />
@@ -11,7 +11,10 @@
   <link rel="preconnect" href="https://fonts.googleapis.com">
   <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
   <link href="https://fonts.googleapis.com/css2?family=Mitr:wght@200;300;400;500;600;700&family=Prompt:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
-    <style>
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Pridi:wght@200;300;400;500;600;700&display=swap" rel="stylesheet"> 
+<style>
       body {
         background-image: url(asset/Banner.png);
         background-repeat: no-repeat;
@@ -25,4 +28,8 @@
         font-family: "Prompt", sans-serif;
         font-weight: 400;
       }
-    </style>
+      p {
+        font-family: "Pridi", serif;
+        
+      }
+</style>
